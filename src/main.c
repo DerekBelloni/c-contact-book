5b285ff4c4b07fc1db34aff54af5ec7f23794dcc
Derek Belloni <dbelloni@compunet.biz>
2024-04-10 20:26:01 -0600

    first commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int c;
+    char *filepath = NULL;
+    char *updateString = NULL;
+    char *addString = NULL;
+    char *removeString = NULL;
+
+    return 0;
+}
